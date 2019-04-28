81695c3f4a48ffbb824f636ee55d6b435dc06733
huadongzhou <1471570248@qq.com>
2019-04-29 00:33:39 +0800

    影院和我的完成

--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="main">
     <Header title="我的喵喵" />
+    <div id="content">
+      <Login />
+    </div>
     <TabBar />
   </div>
 </template>
@@ -8,11 +11,13 @@
 <script>
 import Header from '@/components/Header'
 import TabBar from '@/components/TabBar'
+import Login from '@/components/Login'
 export default {
   name: 'AppMine',
   components: {
     Header,
-    TabBar
+    TabBar,
+    Login
   }
 }
 </script>
