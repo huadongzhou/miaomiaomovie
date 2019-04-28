82631a7758940b67523aec44256a36d8409512c9
huadongzhou <1471570248@qq.com>
2019-04-28 23:27:41 +0800

    电影界面完成

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main">
+    <Header title="喵喵影院" />
+    <TabBar />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+export default {
+  name: 'AppCinema',
+  components: {
+    Header,
+    TabBar
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵" />
+    <TabBar />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+export default {
+  name: 'AppMine',
+  components: {
+    Header,
+    TabBar
+  }
+}
+</script>
+
+<style>
+</style>
